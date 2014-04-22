726b6ce8b15f263982ea30908b4d4e90b9a73e54
Jacob Binder <JRBinder@bellsouth.net>
2014-04-22 00:11:21 -0400

    Changes how calEvent pushes

--- NewReminder.js
+++ NewReminder.js
@@ -90,7 +90,7 @@ function addCalEvent() {
     if (r == true) 
     {
       var postResult = $.ajax({
-        url: 'https://www.googleapis.com/calendar/v3/calendars/primary/events',
+        url: '/calendar/v3/calendars/primary/events',
         type: 'POST',
         dataType: 'json',
         data: {
