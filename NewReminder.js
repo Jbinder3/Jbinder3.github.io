f4b80329560bdeeb69f03bef01e505cc81b6f1c7
Jacob Binder <JRBinder@bellsouth.net>
2014-04-22 00:01:05 -0400

    checkin gsomething

--- NewReminder.js
+++ NewReminder.js
@@ -1,14 +1,6 @@
 var currentUser = null;
 var g_calendarObject = null;
 
-var remindersName = null;
-var category = null;
-var dueDate = null;
-var recurrence = null;
-var importance = null;
-var shoppingSite = null;
-var description = null;
-
 window.onload = function() {
     currentUser = getCurrentUser();
     document.getElementById("imagepreview").style.visibility="hidden";
@@ -49,7 +41,7 @@ window.onload = function() {
 function getCurrentUser() {
     var allcookies = document.cookie;
     // Get all the cookies pairs in an array
-    cookiearray  = allcookies.split(';');
+    cookiearray = allcookies.split(';');
     // Now take key value pair out of this array
     for(var i=0; i<cookiearray.length; i++){
         var name = cookiearray[i].split('=')[0];
@@ -63,13 +55,13 @@ function getCurrentUser() {
 }
 
 function addReminder() {
-    remindersName = document.getElementById("NameTextField").value;
-    category = document.getElementById("CategoryInput").value;
-    dueDate = document.getElementById("DueDateTextField").value;
-    recurrence = document.getElementById("RecurringInput").value;
-    importance = document.getElementById("ImportanceInput").value;
-    shoppingSite = document.getElementById("ShoppingSiteInput").value;
-    description = document.getElementById("DescriptionTextField").value;
+    var remindersName = document.getElementById("NameTextField").value;
+    var category = document.getElementById("CategoryInput").value;
+    var dueDate = document.getElementById("DueDateTextField").value;
+    var recurrence = document.getElementById("RecurringInput").value;
+    var importance = document.getElementById("ImportanceInput").value;
+    var shoppingSite = document.getElementById("ShoppingSiteInput").value;
+    var description = document.getElementById("DescriptionTextField").value;
     
     var postResult = $.ajax({
                             url: 'http://dev.m.gatech.edu/d/tross32/w/remindme/c/api/tasks',
@@ -78,6 +70,7 @@ function addReminder() {
                             data: "username="+currentUser+"&name="+remindersName+"&importance="+importance+"&category="+category+"&duedate="+dueDate+"&recurring="+recurrence+"&shoppingsite="+shoppingSite+"&description="+description,
                             success: function(data) {
                                 addCalEvent();
+                                history.back()
                             },
                             error: function(xhr, textStatus, errorThrown){
                                 alert('Request failed: ' + textStatus + '; ' + errorThrown);
@@ -87,55 +80,40 @@ function addReminder() {
 
 function addCalEvent() {
     var r = confirm("Add reminder as a Google Calendar calendar?");
-    if (r == true) 
-    {
-      var postResult = $.ajax({
-        url: 'https://www.googleapis.com/calendar/v3/calendars/primary/events',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-              "end": {
-              "date": "2014-04-22"
-              },
-              "start": {
-              "date": "2014-04-22"
-              },
-              "summary": "GO BANANAS",
-              "recurrence": [
-              "RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR",
-              "",
-              "",
-              ""
-              ]
-              },
-        success: function(data) {
-            history.back()
-        },
-        error: function(xhr, textStatus, errorThrown){
-            alert('Request failed: Calendar error');
-        }
-    });
-
-      /*
-        POST https://www.googleapis.com/calendar/v3/calendars/primary/events
-        {
-            {
-              "end": {
-              "date": "2014-04-22"
-              },
-              "start": {
-              "date": "2014-04-22"
-              },
-              "summary": "GO BANANAS",
-              "recurrence": [
-              "RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR",
-              "",
-              "",
-              ""
-              ]
-              }
-        }
-        */
-    }
-    addReminder();
+    /*
+if (r == true) {
+var remindersName = document.getElementById("NameTextField").value;
+var dueDate = document.getElementById("DueDateTextField").value;
+dueDate.replace('-', '');
+var recurrence = document.getElementById("RecurringInput").value;
+// Create the calendar service object
+var calendarService = new google.gdata.calendar.CalendarService('GoogleInc-jsguide-1.0');
+// The default "private/full" feed is used to insert event to the
+// primary calendar of the authenticated user
+var feedUri = 'https://www.google.com/calendar/feeds/default/private/full';
+// Create an instance of CalendarEventEntry representing the new event
+var entry = new google.gdata.calendar.CalendarEventEntry();
+// Set the title of the recurring event
+entry.setTitle(google.gdata.Text.create(remindersName));
+// Set up the recurring details using an ical string (RFC 2445 http://www.ietf.org/rfc/rfc2445.txt)
+var recurrence = new google.gdata.Recurrence();
+var icalBreak = '\r\n';
+var recurrenceString = 'DTSTART;TZID=America/New_York:'+dueDate+'T080000' + icalBreak +
+'DTEND;TZID=America/New_York:'+dueDate+'T090000' + icalBreak +
+'RRULE:FREQ=WEEKLY;UNTIL=20211230T230000Z;';
+recurrence.setValue(recurrenceString);
+entry.setRecurrence(recurrence);
+// This callback method that will be called after a successful insertion from insertEntry()
+var callback = function(result) {
+alert('Event created!');
+}
+// Error handler will be invoked if there is an error from insertEntry()
+var handleError = function(error) {
+alert(error);
+}
+// Submit the request using the calendar service object
+calendarService.insertEntry(feedUri, entry, callback,
+handleError, google.gdata.calendar.CalendarEventEntry);
+}
+*/
 }
